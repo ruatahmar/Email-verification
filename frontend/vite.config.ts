a4418fe5c53472af28a99ad2f999caf42e8d677c
Ruata Hmar <hvanlalruata511@gmail.com>
2026-03-14 11:00:39 +0530

    bruh 2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-	base: "/fast/",
 	build: {
 		outDir: '../backend/public',
 		emptyOutDir: true,
